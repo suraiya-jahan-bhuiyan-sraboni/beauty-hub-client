ae9ad9c6922d9184992c99f415d31ff0b4b0cd6a
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-07-01 14:12:13 +0600

    modifing designs

--- src/components/home components/Banner.jsx
+++ src/components/home components/Banner.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 
 const Banner = () => {
   return (
-      <div><div className='w-full relative'>
-          <img className='w-full h-[400px]' src="https://techsquadteam.com/assets/profile/blogimages/15ef18d25c3c9cfef0b0aff23927d6ab.png" alt="" />
-          <div className='absolute right-1/2 bottom-1/4 
-                    transform translate-x-1/2 text-white text-center flex flex-col items-center gap-2 sm:gap-5'>
-              <h1 className='text-md sm:text-3xl lg:text-5xl font-bold text-shadow-lg/50 text-shadow-pink-500  text-cyan-500 '>Premium Beauty & Wellness Services</h1>
-              <p className='text-xs font-bold text-shadow-lg/30 text-shadow-cyan-500 '>Transform your look with certified beauty professionals and luxury treatments</p>
-              <button className='btn bg-cyan-500'>Book Now</button>
+      <div className=''>
+          <div className='w-full relative '>
+              <div className='absolute top-0 left-0 w-full h-full bg-black opacity-60'>
+              </div>
+              <div className='absolute top-1/2 left-1/2 z-20 w-11/12
+  -translate-x-1/2 -translate-y-1/2 text-white text-center flex flex-col items-center gap-7 sm:gap-10'>
+                      <h1 className='w-full text-3xl sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold text-shadow-lg/50 text-shadow-pink-500  text-white '>Premium Beauty & Wellness Services</h1>
+                      <p className='w-full text-xs xl:text-lg font-bold text-shadow-lg/30 text-shadow-pink-500 '>Transform your look with certified beauty professionals and luxury treatments</p>
+                      <button className='btn bg-cyan-500 xl:text-lg'>Book Now</button>
+                  </div>
+          <img className='w-full h-[400px] 2xl:h-[600px] object-cover' src="https://techsquadteam.com/assets/profile/blogimages/15ef18d25c3c9cfef0b0aff23927d6ab.png" alt="" />
+
           </div>
-      </div></div>
+      </div>
   )
 }
 
